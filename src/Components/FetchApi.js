14f59cfd9234ed2afb1275c81e116421d564f1bc
vivekdhumal28 <vivekdhumal.28@gmail.com>
2024-01-04 20:18:22 +0530

    Fetch API

--- src/Components/FetchApi.js
+++ src/Components/FetchApi.js
@@ -1,7 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+const Product = ({ post }) => {
+  return (
+    <>
+      <div>
+        <p>{post.title}</p>
+      </div>
+    </>
+  );
+};
 
 const FetchApi = () => {
-  return <div>FetchApi</div>;
+  const [posts, setPosts] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(`HTTP error! Status ${res.status}`);
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setPosts(data);
+      })
+      .catch((err) => {
+        setError(error);
+      });
+
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <>
+      <div className="App">
+        <div>
+          <h1>Fetch API Component</h1>
+        </div>
+        <h4>Product List</h4>
+        <div className="Product-List">
+          {posts.slice(0, 10).map((post) => (
+            <Product key={post.id} post={post} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default FetchApi;
